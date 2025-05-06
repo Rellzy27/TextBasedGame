623a0f313842a7aa433c8eed71d5bb8d3662b0c7
aldo3241 <aldominotaur123@gmail.com>
2025-05-06 09:30:29 +0700

    Adding Vsync

--- Scripts/Menu/menu.gd
+++ Scripts/Menu/menu.gd
@@ -4,12 +4,8 @@ extends Control
 func _on_start_buttton_pressed() -> void:
 	get_tree().change_scene_to_file("res://Game.tscn")
 
-
-
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Settings.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
--- Scripts/Menu/settings.gd
+++ Scripts/Menu/settings.gd
@@ -1,5 +1,16 @@
 extends Control
 
 
-func _on_check_button_toggled(toggled_on: bool) -> void:
-	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+func _on_vsync_toggled(toggled_on: bool) -> void:
+	var current_vsync_mode = ProjectSettings.get_setting("display/window/vsync_mode")
+	print(current_vsync_mode)
+	if toggled_on:
+		ProjectSettings.set_setting("display/window/vsync_mode", "1")
+		print(current_vsync_mode)
+	else:
+		ProjectSettings.set_setting("display/window/vsync_mode", "0")
+		print(current_vsync_mode)
+	ProjectSettings.save()
+	
+
+	pass # Replace with function body.
